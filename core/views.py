b2b6563d60a403f1144e5233d4a8386b9017cfdf
AndreaIo <ioannis.andreadakis@partners.basf.com>
2026-05-01 22:02:42 +0300

    update homepage stzle 2

--- core/views.py
+++ core/views.py
@@ -1,35 +1,35 @@
 from django.shortcuts import render
 from .models import Profile
-from resume.models import Skill, Experience, Certificate, Education  # <-- Added Education
+from resume.models import Skill, Experience, Certificate
 from projects.models import Project
 
 
 def home(request):
     profile = Profile.objects.first()
 
-    # Projects
-    featured_projects = Project.objects.filter(is_featured=True).order_by('-date_created')[:3]
-
-    # Skills
+    featured_projects = Project.objects.filter(is_featured=True).order_by('-date_created')[:6]
     featured_skills = Skill.objects.filter(is_featured=True)
 
-    # Experience (Jobs)
-    latest_experience = Experience.objects.all().order_by('-start_date')[:3]
-
-    # NEW: Education (Your Biotech background + Self Taught journey)
-    education_list = Education.objects.all().order_by('-start_date')
-
-    # NEW: Certificates (The proof of your skills)
-    # We take the top 4 most recent ones for the homepage
-    latest_certs = Certificate.objects.all().order_by('-date_issued')[:4]
+    journey_items = []
+    for exp in Experience.objects.all():
+        journey_items.append({
+            'kind': 'experience',
+            'date': exp.start_date,
+            'obj': exp,
+        })
+    for cert in Certificate.objects.all():
+        journey_items.append({
+            'kind': 'certificate',
+            'date': cert.date_issued,
+            'obj': cert,
+        })
+    journey_items.sort(key=lambda x: x['date'], reverse=True)
 
     context = {
         'profile': profile,
         'featured_projects': featured_projects,
         'featured_skills': featured_skills,
-        'latest_experience': latest_experience,
-        'education_list': education_list,  # <-- Added to context
-        'latest_certs': latest_certs,  # <-- Added to context
+        'journey_items': journey_items,
     }
 
     return render(request, 'core/home.html', context)
